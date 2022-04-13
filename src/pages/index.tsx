67d682b3836c3073cdaa76f8b72aecec900b2915
Allan <allangeek@gmail.com>
2022-04-13 17:29:17 -0300

    Adiciona mais informações na Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, Flex, Text } from '@chakra-ui/react';
 import { Banner } from '../components/Banner';
 import { Header } from '../components/Header';
 import { Info } from '../components/Info';
@@ -9,6 +9,29 @@ export default function Home() {
       <Header />
       <Banner />
       <Info />
+
+      <Flex
+        direction='column'
+        mt={['9', '9', '20']}
+        mb={['6', '6', '13']}
+        align='center'
+        justify='center'
+      >
+        <Box w='90px' borderTop='2px solid #47585B'></Box>
+
+        <Text
+          color='gray.500'
+          fontSize={['xl', 'xl', '4xl']}
+          fontWeight='500'
+          textAlign='center'
+          my={['6', '6', '3.25rem']}
+          px='1rem'
+        >
+          Vamos nessa?
+          <br />
+          Então escolha seu continente
+        </Text>
+      </Flex>
     </Flex>
   );
 }
