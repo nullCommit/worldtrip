52a53662f83a529996fd40fbb648b135d53e7ae6
Allan <allangeek@gmail.com>
2022-04-13 14:18:10 -0300

    Cria componente Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,32 @@
+import { Flex, Grid, Image } from '@chakra-ui/react';
+
+export function Header() {
+  return (
+    <Flex
+      as='header'
+      w='100%'
+      h={['50px', '100px']}
+      mx='auto'
+      px='1rem'
+      align='center'
+      justify='center'
+    >
+      <Grid
+        w='100%'
+        h='100%'
+        maxW='1160px'
+        alignItems='center'
+        justifyContent='center'
+        templateColumns='repeat(3, 1fr)'
+      >
+        <Image
+          src='/images/logo.svg'
+          alt='world trip logo'
+          justifySelf='center'
+          gridColumn='2'
+          w={['81px', '184px']}
+        />
+      </Grid>
+    </Flex>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
+import { Flex } from '@chakra-ui/react';
+import { Header } from '../components/Header';
+
 export default function Home() {
   return (
-    <div>
+    <Flex direction='column'>
+      <Header />
       <h1>Home</h1>
-    </div>
+    </Flex>
   );
 }
